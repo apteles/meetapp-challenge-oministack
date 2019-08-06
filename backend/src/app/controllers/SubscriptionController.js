2aee8c3a7338bb7db6a46b2f7a5151a90151a621
Andre Teles <andre.telestp@gmail.com>
2019-08-06 19:57:33 -0300

    finished feature subscription

--- backend/src/app/controllers/SubscriptionController.js
+++ backend/src/app/controllers/SubscriptionController.js
@@ -1,4 +1,6 @@
-import { parseISO, isBefore } from 'date-fns';
+import { parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
 import Subscription from '../models/Subscription';
 import Meetup from '../models/Meetup';
 import User from '../models/User';
@@ -48,6 +50,21 @@ class SubscriptionController {
       meetup_id: meetup.id,
     });
 
+    await Mail.sendMail({
+      to: `${meetup.user.name} <${meetup.user.email}>`,
+      subject: 'Nova inscrição feita pelo Meetup',
+      template: 'subscription',
+      context: {
+        manager: meetup.user.name,
+        title: meetup.title,
+        location: meetup.location,
+        date: format(meetup.date, " dd 'de' MMMM'", {
+          locale: pt,
+        }),
+        user: user.name,
+        email: user.email,
+      },
+    });
     return res.json(subscription);
   }
 }
---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '587',
+  secure: false,
+  auth: {
+    user: '0d18910082e0c5',
+    pass: 'd1f642da8fc4b7',
+  },
+  default: {
+    from: 'Meetup <noreply@meetup.com>',
+  },
+};
