59ca1dd41ff619ae80158a9436d26daf078da091
Andre Teles <andre.telestp@gmail.com>
2019-08-08 11:04:39 -0300

    added rectotron to project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import './config/ReactotronConfig';
+
 function App() {
     return (
         <Router history={history}>
