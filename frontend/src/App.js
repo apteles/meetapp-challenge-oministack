4d5f20de5b6207617a9d180ca5ebd7f549830523
Andre Teles <andre.telestp@gmail.com>
2019-08-08 15:31:22 -0300

    setup global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
 import './config/ReactotronConfig';
 
 function App() {
     return (
         <Router history={history}>
+            <GlobalStyle />
             <Routes />
         </Router>
     );
