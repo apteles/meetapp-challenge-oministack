a8b3802514158d2f3fd7ddc1c3ff42c431cd441c
Andre Teles <andre.telestp@gmail.com>
2019-08-09 11:28:13 -0300

    setup store with reactotron and saga

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
@@ -6,12 +7,17 @@ import history from './services/history';
 import GlobalStyle from './styles/global';
 import './config/ReactotronConfig';
 
+import store from './store'
+
 function App() {
     return (
-        <Router history={history}>
-            <GlobalStyle />
-            <Routes />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <GlobalStyle />
+                <Routes />
+            </Router>
+        </Provider>
+
     );
 }
 
