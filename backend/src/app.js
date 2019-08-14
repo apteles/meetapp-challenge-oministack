3bfda90bc5da61b688a61d0d9a686d9bc460228a
Andre Teles <andre.telestp@gmail.com>
2019-08-14 11:29:13 -0300

    added route for static files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import routes from './routes';
 import './database';
@@ -13,6 +14,10 @@ class App {
   middlewares() {
     this.server.use(cors());
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
